7ade72eb91475d2f7fed3232015e7762d7a41dec
Anna <anna.arendt@campus.lmu.de>
2020-01-28 12:06:43 +0100

    in der Tabelle sieht man nun auch, in wie vielen Fällen Kinder mehr Konflikte haben als Eltern

--- positionbased.py
+++ positionbased.py
@@ -447,51 +447,83 @@ def percentage_table(p1, p2, k1, k2):
     len_array = len(p1)
 
     #Anzahl, wie oft Kinder weniger Konflikte ggü. der Eltern haben
-    vgl_k1_p1 = 0
-    vgl_k1_p2 = 0
-    vgl_k2_p1 = 0
-    vgl_k2_p2 = 0
-    vgl_k1_beide = 0
-    vgl_k2_beide = 0
+    better_k1_p1 = 0
+    better_k1_p2 = 0
+    better_k2_p1 = 0
+    better_k2_p2 = 0
+    better_k1_beide = 0
+    better_k2_beide = 0
+
+    worse_k1_p1 = 0
+    worse_k1_p2 = 0
+    worse_k2_p1 = 0
+    worse_k2_p2 = 0
+    worse_k1_beide = 0
+    worse_k2_beide = 0
+
 
     for i in range(len(p1)):
         #Kind1 weniger Konflikte als Elternteil1 und/oder Elternteil2
         if k1[i] < p1[i]:
-            vgl_k1_p1 += 1
+            better_k1_p1 += 1
             if k1[i] < p2[i]:
-                vgl_k1_beide += 1
+                better_k1_beide += 1
         if k1[i] < p2[i]:
-            vgl_k1_p2 += 1
+            better_k1_p2 += 1
 
         #Kind2 weniger Konflikte als Elternteil1 und/oder Elternteil2
         if k2[i] < p1[i]:
-            vgl_k2_p1 += 1
+            better_k2_p1 += 1
             if k2[i] < p2[i]:
-                vgl_k2_beide += 1
+                better_k2_beide += 1
         if k2[i] < p2[i]:
-            vgl_k2_p2 += 1
+            better_k2_p2 += 1
 
-    #Berechnung der Prozente
-    k1_vs_p1 = vgl_k1_p1/len_array * 100
-    k1_vs_p2 = vgl_k1_p2 / len_array * 100
-    k2_vs_p1 = vgl_k2_p1 / len_array * 100
-    k2_vs_p2 = vgl_k2_p2 / len_array * 100
-    k1_vs_beide = vgl_k1_beide / len_array * 100
-    k2_vs_beide = vgl_k2_beide / len_array * 100
+        #Kind1 mehr Konflikte als Elternteil1 und/oder Elternteil2
+        if k1[i] > p1[i]:
+            worse_k1_p1 += 1
+            if k1[i] > p2[i]:
+                worse_k1_beide += 1
+        if k1[i] > p2[i]:
+            worse_k1_p2 += 1
 
-    fig = plt.figure(dpi=80)
+        #Kind2 mehr Konflikte als Elternteil1 und/oder Elternteil2
+        if k2[i] > p1[i]:
+            worse_k2_p1 += 1
+            if k2[i] > p2[i]:
+                worse_k2_beide += 1
+        if k2[i] > p2[i]:
+            worse_k2_p2 += 1
+
+    #Berechnung der Prozente, auf zwei Nachkommastellen ge
+    b_k1_vs_p1 = round((better_k1_p1 / len_array * 100),2)
+    b_k1_vs_p2 = round((better_k1_p2 / len_array * 100),2)
+    b_k2_vs_p1 = round((better_k2_p1 / len_array * 100),2)
+    b_k2_vs_p2 = round((better_k2_p2 / len_array * 100),2)
+    b_k1_vs_beide = round((better_k1_beide / len_array * 100),2)
+    b_k2_vs_beide = round((better_k2_beide / len_array * 100),2)
+    w_k1_vs_p1 = round((worse_k1_p1 / len_array * 100),2)
+    w_k1_vs_p2 = round((worse_k1_p2 / len_array * 100),2)
+    w_k2_vs_p1 = round((worse_k2_p1 / len_array * 100),2)
+    w_k2_vs_p2 = round((worse_k2_p2 / len_array * 100),2)
+    w_k1_vs_beide = round((worse_k1_beide / len_array * 100),2)
+    w_k2_vs_beide = round((worse_k2_beide / len_array * 100),2)
+
+    fig = plt.figure()
     title="Durchgänge = "+str(len_array)
     ax = fig.add_subplot(1, 1, 1)
+
     table_data = [
-        ["Kind1 besser als Parent1", str(k1_vs_p1)+"%", vgl_k1_p1 ],
-        ["Kind1 besser als Parent2", str(k1_vs_p2)+"%", vgl_k1_p2],
-        ["Kind2 besser als Parent1", str(k2_vs_p1)+"%", vgl_k2_p1],
-        ["Kind2 besser als Parent2", str(k2_vs_p2)+"%", vgl_k2_p2],
-        ["Kind1 besser als beide", str(k1_vs_beide)+"%", vgl_k1_beide],
-        ["Kind2 besser als beide", str(k2_vs_beide)+"%", vgl_k2_beide]]
-    table = ax.table(cellText=table_data, loc='center', colLabels=[title,"Prozentsatz", "Anzahl"])
-    table.set_fontsize(14)
-    table.scale(1, 4)
+        ["Kind1/Parent1", str(b_k1_vs_p1)+"% / "+str(w_k1_vs_p1)+"%", str(better_k1_p1)+" / "+str(worse_k1_p1) ],
+        ["Kind1/Parent2", str(b_k1_vs_p2)+"% / "+str(w_k1_vs_p2) + "%", str(better_k1_p2)+" / "+str(worse_k1_p2)],
+        ["Kind2/Parent1", str(b_k2_vs_p1)+"% / "+str(w_k2_vs_p1) + "%", str(better_k2_p1)+" / "+str(worse_k2_p1)],
+        ["Kind2/Parent2", str(b_k2_vs_p2)+"% / "+str(w_k2_vs_p2) + "%", str(better_k2_p2)+" / "+str(worse_k2_p2)],
+        ["Kind1/beide", str(b_k1_vs_beide)+"% / "+str(w_k1_vs_beide) + "%", str(better_k1_beide)+" / "+str(worse_k1_beide)],
+        ["Kind2/beide", str(b_k2_vs_beide)+"% / "+str(w_k2_vs_beide) + "%", str(better_k2_beide)+" / "+str(worse_k2_beide)]]
+
+    table = ax.table(cellText=table_data, loc='center', colLabels=[title,"Prozentsatz besser/schlechter", "Anzahl besser/schlechter"])
+    table.scale(1.2, 1.5)
+    table.set_fontsize(20)
     ax.axis('off')
     plt.show()
 
@@ -524,7 +556,7 @@ if __name__ == '__main__':
         COUNTER += 1
 
 
-    show_overall_conflicts(p1, p2, k1, k2)
+    #show_overall_conflicts(p1, p2, k1, k2)
     boxplot(p1, p2, k1, k2)
     percentage_table(p1, p2, k1, k2)
 
