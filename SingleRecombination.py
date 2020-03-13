d28ad66cfc047ae8e0746d8c9b02cb4cdb46003b
Anna <anna.arendt@campus.lmu.de>
2020-03-13 14:03:12 +0100

    2 funktionen zu overallPlots

--- SingleRecombination.py
+++ SingleRecombination.py
@@ -2,6 +2,7 @@ import sys
 import matplotlib.pyplot as plt
 from Chromosome import Chromosome
 from EA import NQueen1
+from Overall_plots import OverallPlots
 
 START_SIZE = 2  # Population size at start.
 MAX_EPOCHS = 0  # Arbitrary number of test cycles. EIGENTLICH AUF 1000 GESETZT!
@@ -19,10 +20,10 @@ class SingleRekomb:
         self.mStartSize = startSize
         self.mEpochs = maxEpochs
         self.mOffspringPerGeneration = generation
-        self.mMinimumShuffles = minShuffles
-        self.mMaximumShuffles = maxShuffles
         self.mPBCMax = pbcMax
         self.mMaxLength = maxLength
+        self.mMinimumShuffles = minShuffles
+        self.mMaximumShuffles = maxShuffles
 
         self.epoch = 0
         self.childCount = 0
@@ -92,7 +93,7 @@ class SingleRekomb:
         popSize = len(self.population)
         for i in range(popSize):
             thisChromo = self.population[i]
-            thisChromo.set_selected(False)
+            Chromosome.set_selected(thisChromo, False)
 
         return
 
@@ -130,105 +131,6 @@ class SingleRekomb:
         plt.title('scatter plot - parents vs offsprings', fontsize=20)
         plt.show()
 
-def boxplot(p1, p2, k1, k2):
-    data_to_plot = [p1, p2, k1, k2]
-
-    fig = plt.figure(1, figsize=(9, 6))
-    ax = fig.add_subplot(111)
-    plt.ylabel("Konflikte")
-    plt.title('parents and offsprings')
-    ax.boxplot(data_to_plot)
-    ax.set_xticklabels(['Parent1', 'Parent2', 'Child1', 'Child2'])
-    plt.show()
-    return
-
-def percentage_table(p1, p2, k1, k2):
-
-    len_array = len(p1)
-
-    #Anzahl, wie oft Kinder weniger Konflikte als Eltern haben
-    better_k1_p1 = 0
-    better_k1_p2 = 0
-    better_k2_p1 = 0
-    better_k2_p2 = 0
-    better_k1_beide = 0
-    better_k2_beide = 0
-
-    worse_k1_p1 = 0
-    worse_k1_p2 = 0
-    worse_k2_p1 = 0
-    worse_k2_p2 = 0
-    worse_k1_beide = 0
-    worse_k2_beide = 0
-
-
-    for i in range(len(p1)):
-        #Kind1 weniger Konflikte als Elternteil1 und/oder Elternteil2
-        if k1[i] < p1[i]:
-            better_k1_p1 += 1
-            if k1[i] < p2[i]:
-                better_k1_beide += 1
-        if k1[i] < p2[i]:
-            better_k1_p2 += 1
-
-        #Kind2 weniger Konflikte als Elternteil1 und/oder Elternteil2
-        if k2[i] < p1[i]:
-            better_k2_p1 += 1
-            if k2[i] < p2[i]:
-                better_k2_beide += 1
-        if k2[i] < p2[i]:
-            better_k2_p2 += 1
-
-        #Kind1 mehr Konflikte als Elternteil1 und/oder Elternteil2
-        if k1[i] > p1[i]:
-            worse_k1_p1 += 1
-            if k1[i] > p2[i]:
-                worse_k1_beide += 1
-        if k1[i] > p2[i]:
-            worse_k1_p2 += 1
-
-        #Kind2 mehr Konflikte als Elternteil1 und/oder Elternteil2
-        if k2[i] > p1[i]:
-            worse_k2_p1 += 1
-            if k2[i] > p2[i]:
-                worse_k2_beide += 1
-        if k2[i] > p2[i]:
-            worse_k2_p2 += 1
-
-    #Berechnung der Prozente, auf zwei Nachkommastellen ge
-    b_k1_vs_p1 = round((better_k1_p1 / len_array * 100),2)
-    b_k1_vs_p2 = round((better_k1_p2 / len_array * 100),2)
-    b_k2_vs_p1 = round((better_k2_p1 / len_array * 100),2)
-    b_k2_vs_p2 = round((better_k2_p2 / len_array * 100),2)
-    b_k1_vs_beide = round((better_k1_beide / len_array * 100),2)
-    b_k2_vs_beide = round((better_k2_beide / len_array * 100),2)
-    w_k1_vs_p1 = round((worse_k1_p1 / len_array * 100),2)
-    w_k1_vs_p2 = round((worse_k1_p2 / len_array * 100),2)
-    w_k2_vs_p1 = round((worse_k2_p1 / len_array * 100),2)
-    w_k2_vs_p2 = round((worse_k2_p2 / len_array * 100),2)
-    w_k1_vs_beide = round((worse_k1_beide / len_array * 100),2)
-    w_k2_vs_beide = round((worse_k2_beide / len_array * 100),2)
-
-    fig = plt.figure()
-    title="Durchgänge = "+str(len_array)
-    ax = fig.add_subplot(1, 1, 1)
-
-    table_data = [
-        ["Kind1/Parent1", str(b_k1_vs_p1)+"% / "+str(w_k1_vs_p1)+"%", str(better_k1_p1)+" / "+str(worse_k1_p1) ],
-        ["Kind1/Parent2", str(b_k1_vs_p2)+"% / "+str(w_k1_vs_p2) + "%", str(better_k1_p2)+" / "+str(worse_k1_p2)],
-        ["Kind2/Parent1", str(b_k2_vs_p1)+"% / "+str(w_k2_vs_p1) + "%", str(better_k2_p1)+" / "+str(worse_k2_p1)],
-        ["Kind2/Parent2", str(b_k2_vs_p2)+"% / "+str(w_k2_vs_p2) + "%", str(better_k2_p2)+" / "+str(worse_k2_p2)],
-        ["Kind1/beide", str(b_k1_vs_beide)+"% / "+str(w_k1_vs_beide) + "%", str(better_k1_beide)+" / "+str(worse_k1_beide)],
-        ["Kind2/beide", str(b_k2_vs_beide)+"% / "+str(w_k2_vs_beide) + "%", str(better_k2_beide)+" / "+str(worse_k2_beide)]]
-
-    table = ax.table(cellText=table_data, loc='center', colLabels=[title,"Prozentsatz besser/schlechter", "Anzahl besser/schlechter"])
-    table.scale(1.2, 1.5)
-    table.set_fontsize(20)
-    ax.axis('off')
-    plt.show()
-
-    return
-
 if __name__ == '__main__':
     COUNTER = 0
     END = 500
@@ -254,8 +156,8 @@ if __name__ == '__main__':
 
         COUNTER += 1
 
-    boxplot(p1, p2, k1, k2)
-    percentage_table(p1, p2, k1, k2)
+    OverallPlots.boxplot(p1, p2, k1, k2)
+    OverallPlots.percentage_table(p1, p2, k1, k2)
 
 
 
